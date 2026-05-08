7a49f4eef290c77bc31d097b34ab565dcea91a29
destroyo123 <36830646+destroyo123@users.noreply.github.com>
2026-05-08 15:28:07 -0700

    Update play-sound.ps1

--- scripts/play-sound.ps1
+++ scripts/play-sound.ps1
@@ -2,9 +2,5 @@ param(
     [string]$SoundFile
 )
 
-Add-Type -AssemblyName presentationCore
-$player = New-Object System.Windows.Media.MediaPlayer
-$player.Open([Uri]::new((Resolve-Path $SoundFile)))
-$player.Play()
-
-Start-Sleep -Seconds 3
+$player = New-Object System.Media.SoundPlayer $SoundFile
+$player.PlaySync()
